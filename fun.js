1a120e75406936f4a488b9684630fc241013a57e
iofjuupasli <iofjuupasli@gmail.com>
2016-08-02 12:31:52 +0300

    client

---
+++ fun.js
@@ -0,0 +1,16 @@
+// 0: x + y
+// 1: ...
+// 2: ...
+// 3: ...
+// 4: ...
+
+const fun = (x, y) => {
+    return x + y
+};
+
+const level = 0;
+
+module.exports = {
+    fun,
+    level
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+const request = require('request-promise');
+const {fun, level} = require('./fun');
+require('colors');
+
+const server = (x, y) => {
+    return request(`http://localhost:3000?level=${level}&x=${x}&y=${y}`)
+        .then(v => +v);
+}
+
+const test = (x, y) => {
+    return server(x, y)
+        .then(serverValue => {
+            const localValue = fun(x, y);
+            if (serverValue !== localValue) {
+                console.error(`f(${x}, ${y}) = local: ${localValue}, server: ${serverValue}`.red);
+            } else {
+                console.log(`f(${x}, ${y}) = ${localValue}`.green);
+            }
+        });
+};
+
+const xs = [0, 1, 2, 3, 4, 5];
+const ys = [0, 1, 2, 3, 4, 5];
+
+xs.forEach(x => {
+    ys.forEach(y => {
+        test(x, y);
+    });
+});
