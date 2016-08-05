f332c56c04f3d9bac2a587777191f1068ba043f3
Raman Kazimirchyk <t2n3qqq@gmail.com>
2016-08-05 14:50:52 +0300

    done

--- fun.js
+++ fun.js
@@ -1,14 +1,14 @@
 // 0: x + y
-// 1: ...
-// 2: ...
-// 3: ...
-// 4: ...
+// 1: x - y
+// 2: x * y
+// 3: Math.pow(x,y) + x
+// 4: x > 3 && y > 0 ? x*6 + y*200 : x*6 + y*20
 
 const fun = (x, y) => {
-    return x + y
+    return x > 3 && y > 0 ? x*6 + y*200 : x*6 + y*20
 };
 
-const level = 0;
+const level = 4;
 
 module.exports = {
     fun,
