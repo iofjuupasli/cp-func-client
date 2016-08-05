c7a6451162246ace2e874e91637946abe4846ebf
tsapa44 <tsapa44@gmail.com>
2016-08-05 14:29:54 +0300

    first

--- fun.js
+++ fun.js
@@ -1,14 +1,17 @@
 // 0: x + y
-// 1: ...
-// 2: ...
-// 3: ...
-// 4: ...
+// 1: x - y
+// 2: x * y
+// 3: x ^ y + x
+// 4:
 
 const fun = (x, y) => {
-    return x + y
+  if (x >= 4) {
+    return y * 200 + x * 6;
+  }
+    return y * 20 + x * 6;
 };
 
-const level = 0;
+const level = 4;
 
 module.exports = {
     fun,
