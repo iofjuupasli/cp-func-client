fe7d96098ae21663bec592ba87c4bc96def0de8c
Kirill Scherbenok <suhorukovrd@gmail.com>
2016-08-05 14:33:32 +0300

    pr

--- fun.js
+++ fun.js
@@ -4,11 +4,28 @@
 // 3: ...
 // 4: ...
 
+// const fun = (x, y) => {
+//     return x + y
+// };
+
+// const fun = (x, y) => {
+//     return x - y
+// };
+
+// const fun = (x, y) => {
+//     return x * y
+// };
+
 const fun = (x, y) => {
-    return x + y
+  let sum;
+  if (x >= 4) {
+    return (y*200) + (x*6);
+  }
+  sum = (y*20) + (x*6);
+  return sum;
 };
 
-const level = 0;
+const level = 4;
 
 module.exports = {
     fun,
